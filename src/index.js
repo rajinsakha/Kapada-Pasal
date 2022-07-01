044a398a84ef8b28f1660484c6001685e2f039dc
Diwash Nembang <nembangd0@gmail.com>
2022-07-01 22:13:17 +0545

    added somewhat working ui for bot

--- src/index.js
+++ src/index.js
@@ -1,22 +1,30 @@
 const actions = [{
-    triggers: ["hello","hi"],
+    triggers: ["hello", "hi"],
     response: "k xa sathi",
 },
 {
 
-    triggers: ["info"],
+    triggers: ["info", "aafno baray"],
     response: "we are kapdapasal",
-}
+},
+    {
+
+        triggers: ["order", "i want to order",'saman kinnu xa'],
+        response: "what do you want to order",
+    }
 
 ]
+let messages = []
 
 const inputEl = document.getElementById("input");
 const sendEl = document.getElementById("send");
-
+const chatDisplay = document.getElementsByClassName("chat-display")[0];
 
 
 sendEl.addEventListener("click", (e) => {
-    let response;
+    const userMsg = createMessage(inputEl.value, 'user');
+    update(userMsg);
+    let response = "Not Programmed for that message";
     actions.forEach(action => {
         action.triggers.forEach(trigger => {
             if (trigger === inputEl.value) {
@@ -25,6 +33,43 @@ sendEl.addEventListener("click", (e) => {
         })
 
     })
+    const botMsg = createMessage(response, "bot");
+    update(botMsg)
+    render(messages);
 
-    alert(response)
+
 })
+
+
+const render = (messages) => {
+    chatDisplay.textContent = "";
+    messages.forEach(message => {
+        const container = document.createElement('div');
+        if (message.type === "user") {
+            console.log(message.type)
+
+            container.textContent = `${message.text} user`;
+            chatDisplay.appendChild(container)
+            container.style.textAlign = "right"
+
+        } else {
+            container.textContent = `${message.text} bot`;
+            chatDisplay.appendChild(container)
+
+        }
+    })
+    return container;
+}
+
+const createMessage = (message, isBot) => {
+    const messageObj = {
+        text: message,
+        type: isBot
+    }
+
+    return messageObj
+}
+
+const update = (createdMessage) => {
+    messages.push(createdMessage);
+}
