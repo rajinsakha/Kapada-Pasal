8c8eb805fe40cd6ac132eaa32f633845390ea9f0
Diwash Nembang <nembangd0@gmail.com>
2022-07-01 18:02:14 +0545

    added simple bot

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-console.log("hello world");
+const actions = [{
+    triggers: ["hello","hi"],
+    response: "k xa sathi",
+},
+{
+
+    triggers: ["info"],
+    response: "we are kapdapasal",
+}
+
+]
+
+const inputEl = document.getElementById("input");
+const sendEl = document.getElementById("send");
+
+
+
+sendEl.addEventListener("click", (e) => {
+    let response;
+    actions.forEach(action => {
+        action.triggers.forEach(trigger => {
+            if (trigger === inputEl.value) {
+                response = action.response
+            }
+        })
+
+    })
+
+    alert(response)
+})
